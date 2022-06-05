603ea92f4d3f0e3a33833fec80181dab13e4b784
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-06-05 17:22:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+let dimArreglo: number = Number(
+  prompt("Ingrese el numero de butacas a consultar")
+);
+let butacasCine: boolean[] = new Array(dimArreglo);
+
+function asignacionButacas() {
+  return Boolean(Math.floor(Math.random() * 2));
+}
+
+function ocupacionButacas(butacas: boolean[]) {
+  let butacasOcupadas = 0;
+  let butacasDesocupadas = 0;
+  for (let indice: number = 0; indice < butacas.length; indice++) {
+    butacas[indice] = asignacionButacas();
+    if (butacas[indice] === false) {
+      butacasDesocupadas += 1;
+    } else {
+      butacasOcupadas += 1;
+    }
+  }
+  console.log(`El numero de butacas ocupadas es ${butacasOcupadas}`);
+  console.log(`El numero de butacas deocupadas es ${butacasDesocupadas}`);
+}
+console.log(`La cantidad de butacas del cine son ${butacasCine}`);
+ocupacionButacas(butacasCine);
